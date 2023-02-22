28c05e8b38ef932860bb6ffa78b19142fb2a99fb
Alex <89264160+Bidlo2D@users.noreply.github.com>
2023-02-23 00:19:35 +0300

    Delete ip.

--- src/components/content/catalog/assortment/item_slot/item/image_api/ImageAPI.jsx
+++ src/components/content/catalog/assortment/item_slot/item/image_api/ImageAPI.jsx
@@ -1,10 +1,9 @@
 import React from "react"
 
 const ImageAPI = (props) => {
-  //const path = "http://192.168.56.1:8080/img/" + props.name
   return (
     <img
-      src={"http://192.168.56.1:8080/img/" + props.name}
+      src={"http://localhost:8080/img/" + props.name}
       className={props.style}
     ></img>
   )
