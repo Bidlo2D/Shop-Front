04ebcf3774eeade8d9e8be00e0b55a802d0f9426
Bidlo2D <gg.wp.1992@inbox.ru>
2023-03-08 20:14:03 +0300

    Change image position in search bar.

--- src/components/content/catalog/search_bar/SearchBarCatalog.jsx
+++ src/components/content/catalog/search_bar/SearchBarCatalog.jsx
@@ -6,12 +6,12 @@ const SearchBarCatalog = (props) => {
   return (
     <form className={props.styles.searchBar} action="">
       <div className={props.styles.wrapper}>
-        <img className={props.styles.imageSearch} src={search} alt="No"></img>
         <input
           className={props.styles.inputText}
           type="search"
           placeholder="Поиск по каталогу"
         />
+        <img className={props.styles.imageSearch} src={search} alt="No"></img>
       </div>
       {props.children}
     </form>
