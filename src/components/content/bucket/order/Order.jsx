5a397c202344cca2e9505e4869ee502f4ff4b7bf
Bidlo2D <gg.wp.1992@inbox.ru>
2023-03-15 19:33:55 +0300

    Added a block with an indication of the absence of goods in the basket and a button to return to the catalog.

--- src/components/content/bucket/order/Order.jsx
+++ src/components/content/bucket/order/Order.jsx
@@ -3,6 +3,7 @@ import React from "react"
 import styles from "./css/Order.module.css"
 import { useSelector } from "react-redux"
 import ItemOrder from "./item_order/ItemOrder"
+import ButtonTextLink from "./../../../ButtonTextLink"
 
 const Order = () => {
   const products = useSelector((state) => {
@@ -11,16 +12,28 @@ const Order = () => {
 
   return (
     <div className={styles.order}>
-      {products.map((item, i) => (
-        <ItemOrder
-          key={i}
-          id={item.id}
-          name={item.name}
-          color={item.color}
-          image={item.image}
-          price={item.price}
-        />
-      ))}
+      {products.length === 0 ? (
+        <div className={styles.nothing}>
+          <div className={styles.text}>
+            <p>В корзине нет товаров</p>
+            <p>Найдите то, что вам нужно в каталоге или при помощи поиска</p>
+          </div>
+          <ButtonTextLink style={styles.buttonNothing} to="/catalog">
+            Вернуться к покупкам
+          </ButtonTextLink>
+        </div>
+      ) : (
+        products.map((item, i) => (
+          <ItemOrder
+            key={i}
+            id={item.id}
+            name={item.name}
+            color={item.color}
+            image={item.image}
+            price={item.price}
+          />
+        ))
+      )}
     </div>
   )
 }
